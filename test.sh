5b776a589f90aeacd10407141dd93da55188345c
Brandon Byskov <brandonbyskov@gmail.com>
2015-04-08 01:00:18 -0400

    trying to get shell script working

--- test.sh
+++ test.sh
@@ -7,11 +7,16 @@ echo "First Parameter : $2"
 data_params="params"
 data_params_ext=".dat"
 executable="./mandelbox "
+z = "0"
 
 
 for ((a=$1; a <= $2 ; a++))
 do
    # echo $executable$data_params$a$data_params_ext
    # eval $executable$data_params 
-   eval $executable$data_params$a$data_params_ext
+   if [[a -lt 10]]; then eval $executable$data_params$z$z$z$a$data_params_ext
+   else if [[a -lt 100]]; then eval $executable$data_params$z$z$a$data_params_ext
+   else if [[a -lt 1000]]; then eval $executable$data_params$z$a$data_params_ext
+   else eval $executable$data_params$a$data_params_ext
+   fi fi fi
 done
